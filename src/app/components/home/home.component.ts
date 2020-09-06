38f0262c4f99b4214cd5b87db559e7330bd65a2b
Facundo Gamond <facugamond@hotmail.com>
2020-09-05 21:16:41 -0300

    Correcto funcionamiento del slider

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public images: any;
   constructor() { }
 
   ngOnInit(): void {
+    
+    this.images = [
+      {path: '../../assets/images/1.jpg'},
+      {path: '../../assets/images/2.jpg'},
+      {path: '../../assets/images/3.jpg'},
+      
+  ]
   }
 
 }
