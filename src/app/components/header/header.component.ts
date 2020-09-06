db0bc5d1db0c22aa640097fb3be6eec60bb01a89
Facundo Gamond <facugamond@hotmail.com>
2020-09-06 00:06:47 -0300

    Se modificaron estilos header para tener una calse active

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,17 +5,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public componentActive: string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   goTo(component){
-    console.log(component)
     $('html, body').animate({
       scrollTop: $(component).offset().top
     }, 1000);
+    this.componentActive=component;
+
   }
 
 }
