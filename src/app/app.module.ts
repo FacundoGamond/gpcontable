bc85ee6a5e508cd7d3ab66c9a2029968bfa388a1
Facundo Gamond <facugamond@hotmail.com>
2020-09-05 23:08:18 -0300

    Se agrego el componente socialmedia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ServicesComponent
+    ServicesComponent,
+    SocialmediaComponent
   ],
   imports: [
     BrowserModule,
